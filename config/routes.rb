be703404d361a97685cc0e069cab14d18de1921c
jevazquezb <je.vazquezb@gmail.com>
2022-10-26 00:27:55 -0500

    Add static route: root/index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'root#index'
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
